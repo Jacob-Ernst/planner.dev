47a01d36354b7e870c9a72f47c1ef0c5d3234d1e
Jacob Ernst <jacobernst@Jacobs-MacBook-Air.local>
2014-08-26 09:35:07 -0500

    Added basic template for the TODO list

--- public/todo_list.php
+++ public/todo_list.php
@@ -1,9 +1,26 @@
+<?PHP
+	var_dump($_POST);
+	var_dump($_GET);
+?>
 <!doctype html>
 <html>
 <head>
-	<title></title>
+	<meta charset="utf-8">
+	<title>Todo list</title>
 </head>
 <body>
+	<h1>TODO List</h1>
+	<ol>
+		<li>Be awesome</li>
+		<li>Keep being awesome</li>
+		<li>Win at life</li>
+	</ol>
+	<form method="POST">
+		<p><h2>New Items</h2>
+			<label><textarea id="added_items" name="added_items" placeholder="add item here"></textarea>Add Item</label></p>
+		<input type="submit">
+	</form>
+	
 
 </body>
 </html>
