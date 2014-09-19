2c02774de3178f2a0982a5d60f4a99be4990c65a
Jacob Ernst <jacobernst@Jacobs-MacBook-Air.local>
2014-09-19 11:34:56 -0500

    Changed the queries to prepared statements and created a form to add parks

--- public/national_parks.php
+++ public/national_parks.php
@@ -1,12 +1,38 @@
 <?php
+require_once '../db_connector.php';
 $offset = isset($_GET['offset']) ? intval($_GET['offset']) : 0;
-$dbc = new PDO('mysql:host=127.0.0.1;dbname=codeup_pdo_test_db', 'codeup', 'codeuprocks');
-$dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$stmt = $dbc->query("SELECT name, location, area_in_acres, date_established FROM national_parks LIMIT 4 OFFSET $offset");
-$parks = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+$stmt = $dbc->prepare("SELECT name, location, area_in_acres, date_established, description FROM national_parks LIMIT :num_of OFFSET :offset");
+$stmt->bindValue(':num_of', 4, PDO::PARAM_INT);
+$stmt->bindValue(':offset', $offset, PDO::PARAM_INT);
+$stmt->execute();
+$parks = $stmt->fetchALL(PDO::FETCH_ASSOC);
 
 $count = $dbc->query('SELECT count(*) FROM national_parks');
 $number = $count->fetchColumn();
+
+$valid = FALSE;
+if (isset($_POST)) {
+    foreach ($_POST as $value ) {
+        if (empty($value)) {
+            $valid = FALSE;
+            break;
+        }
+        else {
+            $valid = TRUE;
+        }
+    }
+}
+
+if($valid) {
+    $stmt = $dbc->prepare('INSERT INTO national_parks (name, location, area_in_acres, date_established, description) VALUES (:name, :location, :area, :date_established, :description)');
+    $stmt->bindValue(':name', $_POST['name'], PDO::PARAM_STR);
+    $stmt->bindValue(':location', $_POST['location'], PDO::PARAM_STR);
+    $stmt->bindValue(':area', $_POST['area'], PDO::PARAM_STR);
+    $stmt->bindValue(':date_established', $_POST['date'], PDO::PARAM_STR);
+    $stmt->bindValue(':description', $_POST['descr'], PDO::PARAM_STR);
+    $stmt->execute();
+}
 ?>
 
 <html>
@@ -30,6 +56,7 @@ $number = $count->fetchColumn();
                 <th>Location</th>
                 <th>Area in Acres</th>
                 <th>Date Established</th>
+                <th>Description</th>
             </tr>
             <?php foreach ($parks as $parkInfo):?>
                 <tr>
@@ -46,6 +73,10 @@ $number = $count->fetchColumn();
                             <?php $date = new DateTime($parkInfo['date_established']);
                                  echo $date->format('l, j F Y');?>
                         </td>
+                        <td>
+                            <?= $parkInfo['description']?>
+                        
+                        </td>
                 </tr>
             <?php endforeach;?>
         </table>
@@ -59,6 +90,31 @@ $number = $count->fetchColumn();
             <li class="next"><a href="?offset=<?=$offset + 4?>" class='btn'>Next</a></li>
         <?php endif;?>
     </ul>
+    </div>
+    <div class='container'>
+    <form role="form" method="POST" action='national_parks.php'>
+      <div class="form-group">
+        <label for="name">Park</label>
+        <input type="text" class="form-control" name='name' id="name" placeholder="name">
+      </div>
+      <div class="form-group">
+        <label for="location">Location</label>
+        <input type="text" class="form-control" name='location' id="location" placeholder="location">
+      </div>
+      <div class="form-group">
+        <label for="area">Area</label>
+        <input type="text" class="form-control" name='area' id="area" placeholder="area">
+      </div>
+      <div class="form-group">
+        <label for="date">Date</label>
+        <input type="text" class="form-control" name='date' id="date" placeholder="date">
+      </div>
+      <div class="form-group">
+        <label for="descr">Description</label>
+        <textarea name="descr" id='descr' class='form-control' placeholder='description'></textarea>
+      </div>
+      <button type="submit" class="btn btn-default">Submit</button>
+    </form>
     </div>
 </body>
 </html>
